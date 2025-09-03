4148a1cc3095ebc9c87da60567c58459cc1e7a8a
HJacobi1 <henrique.jacobi1@gmail.com>
2025-09-03 20:22:38 -0300

    Done with the basics

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+window.addEventListener("load", () => {
+  tasks.forEach((task) => renderItem(task));
+});
+
 const list = document.getElementById("checklist");
 
 const input = document.getElementById("new-item-input");
 
 document.getElementById("add-item-button").addEventListener("click", () => {
-  addItemToList(input.value);
+  task = { id: `item-${Date.now()}`, text: input.value, completed: false };
+
+  tasks.push(task);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  renderItem(task);
   input.value = "";
 });
 
-function addItemToList(item) {
-  if (item.trim() === "") return;
+function renderItem(task) {
+  if (task.text.trim() === "") return;
 
   const listItem = document.createElement("li");
+  listItem.id = task.id;
 
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.id = `item-${Date.now()}`;
+  checkbox.id = `checkbox-${task.id}`;
+
+  if (task.completed) {
+    checkbox.checked = true;
+  }
 
   const label = document.createElement("label");
-  label.textContent = item;
+  label.textContent = task.text;
   label.htmlFor = checkbox.id;
 
   listItem.appendChild(checkbox);
   listItem.appendChild(label);
-  listItem.addEventListener("click", (e) => {
-    if (e.target !== checkbox) checkbox.checked = !checkbox.checked;
+  listItem.addEventListener("click", (li) => {    
+    console.log(li.target.id == listItem.id);
+    if (li.target !== checkbox && li.target.id == listItem.id)
+      checkbox.checked = !checkbox.checked;
   });
 
   list.appendChild(listItem);
