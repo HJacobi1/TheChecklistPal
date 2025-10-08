f5adf5fea853f36c900dbef284c83512f22b2ff6
HJacobi1 <henrique.jacobi1@gmail.com>
2025-10-07 21:40:18 -0300

    Feature - Added new Navy theme and edited old ones.

--- src/pages/Themes.jsx
+++ src/pages/Themes.jsx
@@ -7,12 +7,26 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 function Themes() {
   const [sidebarVisible, setSidebarVisible] = useState(false);
   const themes = [
-    { value: "light", label: "Light Theme" },
-    { value: "dark", label: "Dark Theme" },
-    { value: "beartheme", label: "Bear Theme" },
+    { value: "light", label: "Light" },
+    { value: "dark", label: "Dark" },
+    { value: "navy", label: "Navy" },
+    { value: "beartheme", label: "Bear" },
   ];
 
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+  function getInitialTheme() {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      return "dark";
+    }
+    return "light";
+  }
+
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || getInitialTheme()
+  );
+
   useEffect(() => {
     document.documentElement.className = theme;
     localStorage.setItem("theme", theme);
@@ -28,21 +42,34 @@ function Themes() {
       <Sidebar active={sidebarVisible} />
       <main className="flex justify-center p-6">
         <section className="w-[500px] space-y-4">
-          {themes.map((item) => (
-            <div key={item.value} className="flex items-center">
-              <input
-                type="radio"
-                name="theme"
-                value={item.value}
-                id={item.value}
-                checked={theme === item.value}
-                onChange={(e) => setTheme(e.target.value)}
-              />
-              <label htmlFor={item.value} className="text-tx-card ml-2">
-                {item.label}
-              </label>
-            </div>
-          ))}
+          <div className="space-y-2 p-6 bg-bg-card rounded-md shadow text-xl">
+            <h3 className="text-2xl text-center font-bold text-tx-title mb-5">
+              Select Theme
+            </h3>
+            {themes.map((item) => (
+              <div
+                key={item.value}
+                className="flex items-center bg-bg-comp tx-tx-card p-2 rounded-md hover:bg-bg-comp-hover hover:cursor-pointer transition-all duration-200"
+                onClick={() => setTheme(item.value)}
+              >
+                <input
+                  className="h-4 w-4 cursor-pointer ml-2"
+                  type="radio"
+                  name="theme"
+                  value={item.value}
+                  id={item.value}
+                  checked={theme === item.value}
+                  onChange={(e) => setTheme(e.target.value)}
+                />
+                <label
+                  htmlFor={item.value}
+                  className="text-tx-card ml-2 -translate-y-0.5 hover:cursor-pointer"
+                >
+                  {item.label}
+                </label>
+              </div>
+            ))}
+          </div>
         </section>
       </main>
       <footer className="fixed bottom-0 w-full text-tx-header bg-bg-header p-2 font-bold">
